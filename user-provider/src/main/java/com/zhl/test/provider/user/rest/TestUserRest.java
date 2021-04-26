564f3313aab6490e6ecd648923c8fba93137c7b9
hardrocksman <zhenghl0517@163.com>
2021-04-26 08:46:07 +0800

    fix sentinel

--- user-provider/src/main/java/com/zhl/test/provider/user/rest/TestUserRest.java
+++ user-provider/src/main/java/com/zhl/test/provider/user/rest/TestUserRest.java
@@ -2,6 +2,7 @@ package com.zhl.test.provider.user.rest;
 
 import com.zhl.test.provider.user.UserFeign;
 import com.zhl.test.provider.user.model.UserVo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +12,14 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/user")
+@Slf4j
 public class TestUserRest implements UserFeign {
 
 
     @Override
     @GetMapping(value = "/get/{id}")
     public UserVo get(@PathVariable("id") Integer id) {
+        log.info("调用了呀！！！！");
         return UserVo.builder().id(id.longValue()).name("name-".concat(id.toString())).build();
     }
 
